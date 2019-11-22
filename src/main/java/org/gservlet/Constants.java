fe5f5399de83dcfdb66fac99e0d2246b36c93eb6
mamadou lamine ba <laminba2003@gmail.com>
2019-11-22 18:50:35 +0100

    changing the value of the RELOAD constant.

--- src/main/java/org/gservlet/Constants.java
+++ src/main/java/org/gservlet/Constants.java
@@ -5,7 +5,7 @@ public interface Constants {
 	public static final String SCRIPTS_FOLDER = "scripts";
 	public static final String DATASOURCE = "dataSource";
 	public static final String CONNECTION = "connection";
-	public static final String RELOAD = "servlet.reload";
+	public static final String RELOAD = "gservlet.reload";
 	public static final String CONTEXT_WRAPPER = "contextWrapper";
 	public static final String REQUEST_WRAPPER = "requestWrapper";
 	public static final String SESSION_WRAPPER = "sessionWrapper";
