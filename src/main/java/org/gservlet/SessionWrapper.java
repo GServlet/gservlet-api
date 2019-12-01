3ed1c9fddfb5a527bdff4cd123ca94317fb474a2
mamadou lamine ba <laminba2003@gmail.com>
2019-12-01 20:37:36 +0100

    adding the javadocs to the sources files

--- src/main/java/org/gservlet/SessionWrapper.java
+++ src/main/java/org/gservlet/SessionWrapper.java
@@ -25,12 +25,12 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 
 /**
-* 
-* 
-* 
-* @author Mamadou Lamine Ba
-* 
-*/
+ * 
+ * 
+ * 
+ * @author Mamadou Lamine Ba
+ * 
+ */
 @SuppressWarnings("serial")
 public class SessionWrapper implements HttpSession, Serializable {
 
@@ -40,36 +40,113 @@ public class SessionWrapper implements HttpSession, Serializable {
 		this.session = session;
 	}
 
+	/**
+	 * Returns the object bound with the specified name in this session, or
+	 * <code>null</code> if no object is bound under the name.
+	 *
+	 * @param name a string specifying the name of the object
+	 *
+	 * @return the object with the specified name
+	 *
+	 * @exception IllegalStateException if this method is called on an invalidated
+	 *                                  session
+	 */
 	@Override
 	public Object getAttribute(String name) {
 		return session.getAttribute(name);
 	}
 
+	/**
+	 * Returns an <code>Enumeration</code> of <code>String</code> objects containing
+	 * the names of all the objects bound to this session.
+	 *
+	 * @return an <code>Enumeration</code> of <code>String</code> objects specifying
+	 *         the names of all the objects bound to this session
+	 *
+	 * @exception IllegalStateException if this method is called on an invalidated
+	 *                                  session
+	 */
 	@Override
 	public Enumeration<String> getAttributeNames() {
 		return session.getAttributeNames();
 	}
+
+	/**
+	 *
+	 * Returns the time when this session was created, measured in milliseconds
+	 * since midnight January 1, 1970 GMT.
+	 *
+	 * @return a <code>long</code> specifying when this session was created,
+	 *         expressed in milliseconds since 1/1/1970 GMT
+	 *
+	 * @exception IllegalStateException if this method is called on an invalidated
+	 *                                  session
+	 */
 
 	@Override
 	public long getCreationTime() {
 		return session.getCreationTime();
 	}
 
+	/**
+	 * Returns a string containing the unique identifier assigned to this session.
+	 * The identifier is assigned by the servlet container and is implementation
+	 * dependent.
+	 * 
+	 * @return a string specifying the identifier assigned to this session
+	 */
 	@Override
 	public String getId() {
 		return session.getId();
 	}
 
+	/**
+	 *
+	 * Returns the last time the client sent a request associated with this session,
+	 * as the number of milliseconds since midnight January 1, 1970 GMT, and marked
+	 * by the time the container received the request.
+	 *
+	 * <p>
+	 * Actions that your application takes, such as getting or setting a value
+	 * associated with the session, do not affect the access time.
+	 *
+	 * @return a <code>long</code> representing the last time the client sent a
+	 *         request associated with this session, expressed in milliseconds since
+	 *         1/1/1970 GMT
+	 *
+	 * @exception IllegalStateException if this method is called on an invalidated
+	 *                                  session
+	 */
 	@Override
 	public long getLastAccessedTime() {
 		return session.getLastAccessedTime();
 	}
 
+	/**
+	 * Returns the maximum time interval, in seconds, that the servlet container
+	 * will keep this session open between client accesses. After this interval, the
+	 * servlet container will invalidate the session. The maximum time interval can
+	 * be set with the <code>setMaxInactiveInterval</code> method.
+	 *
+	 * <p>
+	 * A return value of zero or less indicates that the session will never timeout.
+	 *
+	 * @return an integer specifying the number of seconds this session remains open
+	 *         between client requests
+	 *
+	 * @see #setMaxInactiveInterval
+	 */
 	@Override
 	public int getMaxInactiveInterval() {
 		return session.getMaxInactiveInterval();
 	}
 
+	/**
+	 * Returns the ServletContext to which this session belongs.
+	 * 
+	 * @return The ServletContext object for the web application
+	 * @since Servlet 2.3
+	 */
 	@Override
 	public ServletContext getServletContext() {
 		return session.getServletContext();
@@ -96,7 +173,8 @@ public class SessionWrapper implements HttpSession, Serializable {
 	 *
 	 * @return the object with the specified name
 	 *
-	 * @exception IllegalStateException if this method is called on an invalidated  session
+	 * @exception IllegalStateException if this method is called on an invalidated
+	 *                                  session
 	 */
 	@Deprecated
 	public Object getValue(String name) {
@@ -110,18 +188,37 @@ public class SessionWrapper implements HttpSession, Serializable {
 	 * @return an array of <code>String</code> objects specifying the names of all
 	 *         the objects bound to this session
 	 *
-	 * @exception IllegalStateException if this method is called on an invalidated session
+	 * @exception IllegalStateException if this method is called on an invalidated
+	 *                                  session
 	 */
 	@Deprecated
 	public String[] getValueNames() {
 		return session.getValueNames();
 	}
 
+	/**
+	 * Invalidates this session then unbinds any objects bound to it.
+	 *
+	 * @exception IllegalStateException if this method is called on an already
+	 *                                  invalidated session
+	 */
 	@Override
 	public void invalidate() {
 		session.invalidate();
 	}
 
+	/**
+	 * Returns <code>true</code> if the client does not yet know about the session
+	 * or if the client chooses not to join the session. For example, if the server
+	 * used only cookie-based sessions, and the client had disabled the use of
+	 * cookies, then a session would be new on each request.
+	 *
+	 * @return <code>true</code> if the server has created a session, but the client
+	 *         has not yet joined
+	 *
+	 * @exception IllegalStateException if this method is called on an already
+	 *                                  invalidated session
+	 */
 	@Override
 	public boolean isNew() {
 		return session.isNew();
@@ -135,13 +232,31 @@ public class SessionWrapper implements HttpSession, Serializable {
 	 *
 	 * @param value the object to be bound; cannot be null
 	 *
-	 * @exception IllegalStateException if this method is called on an invalidated session
+	 * @exception IllegalStateException if this method is called on an invalidated
+	 *                                  session
 	 */
 	@Deprecated
 	public void putValue(String name, Object value) {
 		session.putValue(name, value);
 	}
 
+	/**
+	 * Removes the object bound with the specified name from this session. If the
+	 * session does not have an object bound with the specified name, this method
+	 * does nothing.
+	 *
+	 * <p>
+	 * After this method executes, and if the object implements
+	 * <code>HttpSessionBindingListener</code>, the container calls
+	 * <code>HttpSessionBindingListener.valueUnbound</code>. The container then
+	 * notifies any <code>HttpSessionAttributeListener</code>s in the web
+	 * application.
+	 *
+	 * @param name the name of the object to remove from this session
+	 *
+	 * @exception IllegalStateException if this method is called on an invalidated
+	 *                                  session
+	 */
 	@Override
 	public void removeAttribute(String name) {
 		session.removeAttribute(name);
@@ -153,18 +268,57 @@ public class SessionWrapper implements HttpSession, Serializable {
 	 *
 	 * @param name the name of the object to remove from this session
 	 *
-	 * @exception IllegalStateException if this method is called on an invalidated session
+	 * @exception IllegalStateException if this method is called on an invalidated
+	 *                                  session
 	 */
 	@Deprecated
 	public void removeValue(String name) {
 		session.removeValue(name);
 	}
 
+	/**
+	 * Binds an object to this session, using the name specified. If an object of
+	 * the same name is already bound to the session, the object is replaced.
+	 *
+	 * <p>
+	 * After this method executes, and if the new object implements
+	 * <code>HttpSessionBindingListener</code>, the container calls
+	 * <code>HttpSessionBindingListener.valueBound</code>. The container then
+	 * notifies any <code>HttpSessionAttributeListener</code>s in the web
+	 * application.
+	 * 
+	 * <p>
+	 * If an object was already bound to this session of this name that implements
+	 * <code>HttpSessionBindingListener</code>, its
+	 * <code>HttpSessionBindingListener.valueUnbound</code> method is called.
+	 *
+	 * <p>
+	 * If the value passed in is null, this has the same effect as calling
+	 * <code>removeAttribute()</code>.
+	 *
+	 *
+	 * @param name  the name to which the object is bound; cannot be null
+	 *
+	 * @param value the object to be bound
+	 *
+	 * @exception IllegalStateException if this method is called on an invalidated
+	 *                                  session
+	 */
 	@Override
 	public void setAttribute(String name, Object value) {
 		session.setAttribute(name, (Serializable) value);
 	}
 
+	/**
+	 * Specifies the time, in seconds, between client requests before the servlet
+	 * container will invalidate this session.
+	 *
+	 * <p>
+	 * An <tt>interval</tt> value of zero or less indicates that the session should
+	 * never timeout.
+	 *
+	 * @param interval An integer specifying the number of seconds
+	 */
 	@Override
 	public void setMaxInactiveInterval(int interval) {
 		session.setMaxInactiveInterval(interval);
