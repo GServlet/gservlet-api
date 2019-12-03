3ca232b9bba9b3baae19cbaa90cedacb7c945d2e
mamadou lamine ba <laminba2003@gmail.com>
2019-12-03 11:37:24 +0100

    adding the javadocs to the sources files

---
+++ src/main/java/org/gservlet/annotation/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains a number of annotations that allow users to use annotations to declare servlets, filters, listeners and specify the metadata for the declared component.
+ * 
+ * @see javax.servlet.annotation
+ * 
+ */
+package org.gservlet.annotation;
---
+++ src/main/java/org/gservlet/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains a number of classes and interfaces to make the use of the Groovy language possible in order to create servlets, filters, listeners.
+ * 
+ * @see javax.servlet
+ * 
+ */
+package org.gservlet;
