8afb30fb16d2dba01c7cfce6013343e590e38632
mamadou lamine ba <laminba2003@gmail.com>
2020-01-04 18:22:47 +0100

    updating the StartupListener class

--- src/test/java/org/gservlet/DatabaseManagerTest.java
+++ src/test/java/org/gservlet/DatabaseManagerTest.java
@@ -3,10 +3,12 @@ package org.gservlet;
 import static org.gservlet.Constants.*;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import java.io.File;
 import java.io.FileReader;
 import java.util.Properties;
 import javax.servlet.ServletContext;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.junit.Test;
 
 public class DatabaseManagerTest {
@@ -15,7 +17,9 @@ public class DatabaseManagerTest {
 	public void loadConfiguration() throws Exception {
 		File folder = new File("src/test/resources/" + CONFIG_FOLDER);
 		assertEquals(true, folder.exists());
-		DatabaseManager databaseManager = new DatabaseManager(mock(ServletContext.class));
+		ServletContext context = mock(ServletContext.class);
+		when(context.getAttribute(DATASOURCE)).thenReturn(new BasicDataSource());
+		DatabaseManager databaseManager = new DatabaseManager(context);
 		File file = new File(folder + "/" + APP_CONFIG_FILE);
 		Properties configuration = new Properties();
 		FileReader reader = new FileReader(file);
