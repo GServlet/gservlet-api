0b8daed9ee539b0fa6c23513c47662e0a836fe88
mamadou lamine ba <laminba2003@gmail.com>
2020-01-09 23:29:51 +0100

    updating the HttpFilter class

--- src/test/resources/scripts/HttpFilter.groovy
+++ src/test/resources/scripts/HttpFilter.groovy
@@ -1,22 +1,26 @@
 import org.gservlet.annotation.Filter
 import org.gservlet.annotation.InitParam
+import org.gservlet.AbstractFilter
 
 @Filter(value="/*", urlPatterns = ['/test/*'],
 initParams = [
 	@InitParam(name = "location", value = "D:/Uploads"),
 	@InitParam(name = "maxUploadSize", value = "9900000")
 ])
-class HttpFilter {
+class HttpFilter extends AbstractFilter {
 	
 	void init() {
+		super.init()
 		request.setAttribute("state","init")
 	}
 	
 	void filter() {
+		super.filter()
 		request.setAttribute("state","filtering")
 	}
 	
 	void destroy() {
+		super.destroy()
 		request.setAttribute("state","destroy")
 	}
 	
