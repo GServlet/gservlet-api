913a52bcc6934c5bf647dd7d4aa3e5c91a0fb2e9
mamadou lamine ba <laminba2003@gmail.com>
2020-10-19 22:29:03 +0100

    improving the API

--- src/main/java/org/gservlet/DefaultFilterConfig.java
+++ src/main/java/org/gservlet/DefaultFilterConfig.java
@@ -1,11 +1,25 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
 package org.gservlet;
 
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
 import javax.servlet.FilterConfig;
-import javax.servlet.ServletContext;
 
 /**
 * 
@@ -14,17 +28,8 @@ import javax.servlet.ServletContext;
 * @author Mamadou Lamine Ba
 * 
 */
-public class DefaultFilterConfig implements FilterConfig {
+public class DefaultFilterConfig extends AbstractConfig implements FilterConfig {
 
-	/**
-	 * The initialization parameters Map
-	 */
-	private final Map<String, String> parameters = new HashMap<>();
-
-	/**
-	 * The servlet context
-	 */
-	private ServletContext servletContext;
 	
 	/**
 	 * Returns the name of this filter instance.
@@ -38,67 +43,5 @@ public class DefaultFilterConfig implements FilterConfig {
 		return null;
 	}
 
-	/**
-	 * Returns a reference to the ServletContext in which the caller is executing.
-	 *
-	 * @return a ServletContext object, used by the caller to interact with its servlet container
-	 * 
-	 * 
-	 */
-	@Override
-	public ServletContext getServletContext() {
-		return servletContext;
-	}
 	
-	/**
-	 * 
-	 * Sets the servlet context
-	 * 
-	 * @param servletContext the servlet context
-	 * 
-	 */
-	public void setServletContext(ServletContext servletContext) {
-		this.servletContext = servletContext;
-	}
-
-
-	/**
-	 * 
-	 * Sets an initialization parameter
-	 * 
-	 * @param name  the initialization parameter name
-	 * @param value the initialization parameter value
-	 * 
-	 */
-	public void addInitParameter(String name, String value) {
-		parameters.put(name, value);
-	}
-	
-	/**
-	 * Gets the value of the initialization parameter with the given name.
-	 *
-	 * @param name the name of the initialization parameter whose value to get
-	 *
-	 * @return a <code>String</code> containing the value of the initialization
-	 *         parameter, or <code>null</code> if the initialization parameter does
-	 *         not exist
-	 */
-	@Override
-	public String getInitParameter(String name) {
-		return parameters.get(name);
-	}
-
-	/**
-	 * Returns the names of the servlet's initialization parameters as an
-	 * <code>Enumeration</code> of <code>String</code> objects, or an empty
-	 * <code>Enumeration</code> if the filter has no initialization parameters.
-	 *
-	 * @return an <code>Enumeration</code> of <code>String</code> objects containing
-	 *         the names of the filter's initialization parameters
-	 */
-	@Override
-	public Enumeration<String> getInitParameterNames() {
-		return Collections.enumeration(parameters.keySet());
-	}
-
 }
